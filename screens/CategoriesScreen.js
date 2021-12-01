c30361f25565a266a7eab4b26773b5a4339d2188
nguye374 <nguye374@sheridancollege.ca>
2021-12-01 00:20:26 -0500

    Dec 1 start a new Meal project

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function CategoriesScreen(props) {
+  return <div></div>;
+}
+
+const styles = StyleSheet.create({});
