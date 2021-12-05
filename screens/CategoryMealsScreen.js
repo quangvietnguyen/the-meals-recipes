2fed5aee03357e7bf715c8382c97f75c240ee9f1
nguye374 <nguye374@sheridancollege.ca>
2021-12-05 02:14:00 -0500

    Dec 5 add the navigation for the Category Meal

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 
 export default function CategoryMealsScreen(props) {
   return (
     <View style={styles.screen}>
       <Text>The Categories Meal Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+          });
+        }}
+      ></Button>
     </View>
   );
 }
