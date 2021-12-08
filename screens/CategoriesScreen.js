4cc0ef18130485e4e153113319a9f09ede3bc07e
nguye374 <nguye374@sheridancollege.ca>
2021-12-07 20:38:21 -0500

    Dec 7 output grid from data and models

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, FlatList } from "react-native";
 import CategoryMealsScreen from "./CategoryMealsScreen";
+
+import { CATEGORIES } from "../data/data";
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 export default function CategoriesScreen(props) {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button
-        title="Go to Meals"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
-        }}
-      />
-    </View>
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+    // <View style={styles.screen}>
+    //   <Text>The Categories Screen</Text>
+    //   <Button
+    //     title="Go to Meals"
+    //     onPress={() => {
+    //       props.navigation.navigate({ routeName: "CategoryMeals" });
+    //     }}
+    //   />
+    // </View>
   );
 }
 
@@ -21,5 +37,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
